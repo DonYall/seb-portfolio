69c9988953bf32e167e4902cc1a4741c8b83a3b4
DonYall <highvoltage9886@gmail.com>
2025-04-01 12:57:31 -0400

    clean up workspace

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 function App() {
   return (
     <>
-      <div>
-        <div id="stars"></div>
-        <div id="stars2"></div>
-        <div id="stars3"></div>
-      </div>
+      <div></div>
     </>
   );
 }
