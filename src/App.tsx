2f1dcebe85effb7fc6f257ed4f32cdb4caa277ac
DonYall <highvoltage9886@gmail.com>
2025-04-01 12:56:18 -0400

    first

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
